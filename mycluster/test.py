37a004dace83163dcb20755ae3d452c9b260e0b3
Jamil Appa <jamil.appa@zenotech.com>
2014-06-10 15:31:14 +0100

    bug fix

--- mycluster/test.py
+++ mycluster/test.py
@@ -2,10 +2,10 @@
 
 import mycluster
 
-mycluster.create_submit('hybrid:hybrid.q',script_name='test.job',{'num_tasks': 2,
-                                           'tasks_per_node':2,
-                                           'my_script':'test.bsh',
-                                           })
+mycluster.create_submit('hybrid:hybrid.q',script_name='test.job',num_tasks=2,
+                                           tasks_per_node=2,
+                                           my_script='test.bsh',
+                                           )
 
 mycluster.submit('test.job')
 
