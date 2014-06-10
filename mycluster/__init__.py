37b1a09332135718b892c5d6bdd5e37becb0f153
Jamil Appa <jamil.appa@zenotech.com>
2014-06-10 15:06:57 +0100

    Added persistence

--- mycluster/__init__.py
+++ mycluster/__init__.py
@@ -1,9 +1,10 @@
 import os
-
+import time
 
 JOB_SCHEDULERS = ('SGE','SLURM','LSF','PBS','TORQUE','MAUI','LOADLEVELER')
 
 scheduler = None 
+job_db = None
 
 def detect_scheduling_sys():
 
@@ -29,7 +30,13 @@ def create_submit(queue_id,script_name=None,**kwargs):
     return script
 
 def submit(script_name):
-    return scheduler.submit(script_name)
+    job_id = scheduler.submit(script_name)
+    if job_db != None:
+        from mycluster.persist import Job
+        job = Job(job_id,time.time(),'sge')
+        job_db.add(job)
+        
+    return job_id
 
 def my_import(name):
     mod = __import__(name)
@@ -38,10 +45,34 @@ def my_import(name):
         mod = getattr(mod, comp)
     return mod
 
+def get_directory():
+    from os.path import expanduser
+    home = expanduser("~")
+    directory = home+'.mycluster'
+    return directory
+
+def create_directory():
+    directory = get_directory()
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def create_db():
+    global job_db
+    try:
+        from mycluster.persist import JobDB
+        job_db = JobDB()    
+    except:
+        pass
+        
+def init():
+    global scheduler
+    create_directory()
+    create_db()
+    scheduler = detect_scheduling_sys()
 """
 Module initialiser functions
 """
-scheduler = detect_scheduling_sys()
+init()
 
 if __name__ == "__main__":
     queues()
---
+++ mycluster/persist/__init__.py
@@ -0,0 +1,49 @@
+import ZODB
+from persistent import Persistent
+from ZODB import FileStorage, DB
+from mycluster import get_directory
+from BTrees.OOBTree import OOBTree
+import transaction
+
+class JobDB(object):
+        
+    def __init__(self):
+    
+        directory = get_directory()
+        self.storage = FileStorage.FileStorage(directory+'db.fs')
+        self.db = DB(self.storage)
+        self.connection = self.db.open()
+        
+        dbroot = self.connection.root()
+
+        # Ensure that a 'job_db' key is present
+        # in the root
+        if not dbroot.has_key('job_db'):
+            from BTrees.OOBTree import OOBTree
+            dbroot['job_db'] = OOBTree()
+
+        self.job_db = dbroot['job_db']
+
+    def add(self,job):
+        self.job_db[job.id] = job
+        transaction.commit()
+        
+    def get(self,job_id):
+        return self.job_db[job_id]
+            
+    def close(self):
+        self.connection.close()
+        self.db.close()
+        self.storage.close()
+
+class Job(Persistent):
+    def __init__(self, job_id, time_stamp, scheduler):
+        self.id     = job_id
+        self.time_stamp = time_stamp
+        self.scheduler  = scheduler
+        self.status     = 'submitted'
+        
+    def update_status(self,new_status):
+        self.status     = new_status
+        self._p_changed = 1
+        
