4d413d45def838d730806097484d7ccf9d49744f
Jamil Appa <jamil.appa@zenotech.com>
2016-05-03 16:59:14 +0100

    Fix to test code

--- mycluster/test.py
+++ mycluster/test.py
@@ -2,6 +2,8 @@
 
 import mycluster
 
+mycluster.init()
+
 mycluster.create_submit('hybrid:hybrid.q',script_name='test.job',num_tasks=2,
                                                                    tasks_per_node=2,
                                                                    my_script='test.bsh',
